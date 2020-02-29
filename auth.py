e7ed933e09327c79a9bc6c327f62fab635afe181
dwmanguiat <dwmanguiat@gmail.com>
2020-02-29 14:33:20 -0500

    cleaned up CCF token function

--- auth.py
+++ auth.py
@@ -2,28 +2,16 @@ import requests
 import json
 import util
 
-config = util.getConfig()
-keys = config["Keys"]
-print(keys)
-
-
-tokenEndpoint = "https://accounts.spotify.com/api/token"
-playlistEndpoint = "https://api.spotify.com/v1/me/playlists"
-
-params= {
+def getCCFToken(keys):
+    CCFTokenEndpoint = "https://accounts.spotify.com/api/token"
+    params= {
     "client_id": keys["ClientID"]
     ,"client_secret": keys["SecretKey"]
     ,"grant_type": "client_credentials"
-}
+    }
 
-response = requests.post(tokenEndpoint, params)
-payload = json.loads(response.content.decode('utf-8'))
+    response = requests.post(CCFTokenEndpoint, params)
+    payload = json.loads(response.content.decode('utf-8'))
 
-authHeaders = {"Authorization": "Bearer {}".format(payload["access_token"])}
-print(authHeaders)
+    return payload
 
-response2 = requests.get(playlistEndpoint, headers=authHeaders)
-
-payload2 = json.loads(response2.content.decode('utf-8'))
-
-print(payload2)
